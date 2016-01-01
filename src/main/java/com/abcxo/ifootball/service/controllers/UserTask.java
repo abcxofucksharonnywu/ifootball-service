e310d30fcecfcca113e714fcfe4fde8a34787480
xiedao.xd <xiedao.xd@alibaba-inc.com>
2016-01-01 19:00:56 +0800

    wefwefwe

--- src/main/java/com/abcxo/ifootball/service/controllers/UserTask.java
+++ src/main/java/com/abcxo/ifootball/service/controllers/UserTask.java
@@ -37,7 +37,7 @@ public class UserTask implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        runInitUsers();
+//        runInitUsers();
     }
 
     public void runInitUsers() {
