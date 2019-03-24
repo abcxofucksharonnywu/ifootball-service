94515a9eac7f02767be33ee5c46e527470676eec
shadow <shashadowdow@gmail.com>
2019-03-24 15:13:32 +0800

    wfwef

--- src/main/java/com/abcxo/ifootball/service/Application.java
+++ src/main/java/com/abcxo/ifootball/service/Application.java
@@ -7,8 +7,12 @@ package com.abcxo.ifootball.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.servlet.MultipartConfigElement;
 
 //@SpringBootApplication
 //@EnableScheduling
@@ -31,4 +35,19 @@ public class Application extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }
+
+    /**
+     * 配置上传文件大小的配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //  单个数据大小
+        factory.setMaxFileSize("102400KB");
+        /// 总上传数据大小
+        factory.setMaxRequestSize("102400KB");
+        return factory.createMultipartConfig();
+    }
+
 }
